66af61d4dbaedb8d77b459f11a7a12374f8206f8
robertbruzualpatino-oss <rbruzualpatio@byupathway.edu>
2026-05-11 11:40:39 -0400

    Changed Arrays.cs

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -12,8 +12,14 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        double[] results = new double[length];
 
-        return []; // replace this return statement with your own
+        for (int i = 0; i < length; i++)
+        {
+            results[i] = number * (i + 1);
+        }
+
+        return results; // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +35,12 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+        int splitIndex = data.Count - amount;
+
+        List<int> rightPart = data.GetRange(splitIndex, amount);
+
+        data.RemoveRange(splitIndex, amount);
+
+        data.InsertRange(0, rightPart);
     }
 }
