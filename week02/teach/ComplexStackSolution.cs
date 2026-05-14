a000005d63352075cf8c93d2759f44f40e35b4cc
robertbruzualpatino-oss <rbruzualpatio@byupathway.edu>
2026-05-14 17:17:40 -0400

    fixed ComplexStackSolution.cs

--- week02/teach/ComplexStackSolution.cs
+++ week02/teach/ComplexStackSolution.cs
@@ -1,5 +1,9 @@
-﻿public static class ComplexStackSolution {
-    public static void Main() {
+﻿using System.Collections.Generic;
+
+public static class ComplexStackSolution
+{
+    public static void Run()
+    {
         // True (stack was empty at the end)
         Console.WriteLine(CheckBraces("(a == 3 or (b == 5 and c == 6))"));
         // False ...wrong opening square bracket (stack had only '(' in it before it was popped and compared with ']')
@@ -7,24 +11,30 @@
         Console.WriteLine(CheckBraces("(students]i].Grade > 80 and students[i].Grade < 90"));
         // False ....missing closing ')' (stack had an extra '(' in it at the end when it was supposed to be empty
         //                 here -------\/
-        Console.WriteLine(CheckBraces("(robot[id + 1].Execute(.Pass() || (!robot[id * (2 + i)].Alive && stormy) || (robot[id - 1].Alive && lavaFlowing))"));
+        Console.WriteLine(CheckBraces("(robot[id + 1].Execute(.Pass() || (!robot[id * (2 + i)].Alive && stormy) || (robot[id - 1].Alive && lavaFlowing)"));
     }
 
-    public static bool CheckBraces(string line) {
+    public static bool CheckBraces(string line)
+    {
         var stack = new Stack<char>();
-        foreach (var item in line) {
-            if (item is '(' or '[' or '{') {
+        foreach (var item in line)
+        {
+            if (item is '(' or '[' or '{')
+            {
                 stack.Push(item);
             }
-            else if (item is ')') {
+            else if (item is ')')
+            {
                 if (stack.Count == 0 || stack.Pop() != '(')
                     return false;
             }
-            else if (item is ']') {
+            else if (item is ']')
+            {
                 if (stack.Count == 0 || stack.Pop() != '[')
                     return false;
             }
-            else if (item is '}') {
+            else if (item is '}')
+            {
                 if (stack.Count == 0 || stack.Pop() != '{')
                     return false;
             }
