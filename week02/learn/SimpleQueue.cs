febc71630696e08c3e3375286e6c1003537d1aaf
robertbruzualpatino-oss <rbruzualpatio@byupathway.edu>
2026-05-14 13:23:17 -0400

    fixed SimpleQueue.cs

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -66,8 +66,9 @@
         if (_queue.Count <= 0)
             throw new IndexOutOfRangeException();
 
-        var value = _queue[1];
-        _queue.RemoveAt(1);
+        var targetIndex = _queue.Count - 1;
+        var value = _queue[targetIndex];
+        _queue.RemoveAt(targetIndex);
         return value;
     }
 }
